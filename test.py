337cbf288cb3bac2f2aac19159eb675dd0c46f92
JOSHUA_LEI <joshualei06@gmail.com>
2019-03-06 19:11:09 +0800

    Second commit, less useless programs

--- test.py
+++ test.py
@@ -4,23 +4,28 @@ import numpy as np
 def nothing(x):
     pass
 
-# Create a black image, a window
-img = np.zeros((300,512,3), np.uint8)
-cv2.namedWindow('image')
+def mask(frame, lower, upper):
+    bgr = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+    lower = np.array(lower)
+    upper = np.array(upper)
+    mask = cv2.inRange(bgr, lower, upper)
+    return mask
 
+cv2.namedWindow('image')
 # create trackbars for color change
-cv2.createTrackbar('R','image',0,255,nothing)
-cv2.createTrackbar('G','image',0,255,nothing)
-cv2.createTrackbar('B','image',0,255,nothing)
+cv2.createTrackbar('R', 'image', 0, 255, nothing)
+cv2.createTrackbar('G', 'image', 0, 255, nothing)
+cv2.createTrackbar('B', 'image', 0, 255, nothing)
 
 # create switch for ON/OFF functionality
 switch = '0 : OFF \n1 : ON'
-cv2.createTrackbar(switch, 'image',0,1,nothing)
+cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+
 
 while(1):
-    cv2.imshow('image',img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
+    img = cv2.imread("img/phot.jpg")
+    cv2.imshow('image', img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
     # get current positions of four trackbars
