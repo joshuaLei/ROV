2abbb0e4d8a93fc29c4ea6c62981391d33152058
JOSHUA_LEI <joshualei06@gmail.com>
2019-04-17 13:58:08 +0800

    new commit

---
+++ Cannon_Task_Calculation.py
@@ -0,0 +1,15 @@
+import os
+import cv2 as cv
+
+m = 0
+
+for roots, subfolders, filenames in os.walk("photos"):
+    for file in filenames:
+        image = file[13:19]
+        m = image
+        if m > image:
+            m = image
+
+print(file)
+
+cv.imread("photos/%s" % file)
